ae72967e63ed4a2184f563b33ad627d74b41104e
Spacecer2 <Fabian_hoess@outlook.de>
2025-11-16 11:37:17 +0100

    Update number_guessing.py

--- number_guessing.py
+++ number_guessing.py
@@ -4,21 +4,33 @@ from time import sleep
 print("Welcome to the Number Guessing Game!")
 sleep(1)
 
-run = True
-
-while run == True:
-
-    guessed_number = int(input(f"Guess a number between 1 and 10.: "))
-
+while True:
     true_number = random.randint(1, 10)
+    print("I have chosen a number between 1 and 10.")
 
-    if guessed_number == true_number:
-        print("You guessed correctly!")
-    else:
-        print(f"Sorry, the correct number was {true_number}.")
+    while True:
+        guess = input("Your guess (or 'exit' to quit): ")
 
-    play_again = input("Do you want to play again? (yes/no): ").lower()
-    if play_again != "yes":
-        run = False
+        if guess.lower() == "exit":
+            print("Goodbye!")
+            exit()
 
-print("Thanks for playing! Goodbye!")
+        try:
+            guess = int(guess)
+        except ValueError:
+            print("Invalid input. Enter a number between 1 and 10.")
+            continue
+
+        if not 1 <= guess <= 10:
+            print("Number out of range.")
+            continue
+
+        if guess == true_number:
+            print("Correct!")
+            break
+        else:
+            print("Nope, try again.")
+
+    if input("Play again? (yes/no): ").lower() != "yes":
+        print("Goodbye!")
+        break
